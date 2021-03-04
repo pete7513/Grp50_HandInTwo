41e62e06f044ab025004c4337d17be2f90efa071
Peter Gehlert Theilgaard <au603039@uni.au.dk>
2021-03-04 13:37:55 +0100

    Create Folder Structure
    Adding all of Franks Classes

--- Grp50_HandInTwo/Grp50_HandInTwo/Program.cs
+++ Grp50_HandInTwo/Grp50_HandInTwo/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
-
-namespace Grp50_HandInTwo
-{
-    class Program
+﻿    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+				// Assemble your system here from all the classes
+
+            bool finish = false;
+            do
+            {
+                string input;
+                System.Console.WriteLine("Indtast E, O, C, R: ");
+                input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input)) continue;
+
+                switch (input[0])
+                {
+                    case 'E':
+                        finish = true;
+                        break;
+
+                    case 'O':
+                        door.OnDoorOpen();
+                        break;
+
+                    case 'C':
+                        door.OnDoorClose();
+                        break;
+
+                    case 'R':
+                        System.Console.WriteLine("Indtast RFID id: ");
+                        string idString = System.Console.ReadLine();
+
+                        int id = Convert.ToInt32(idString);
+                        rfidReader.OnRfidRead(id);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            } while (!finish);
         }
     }
 }
