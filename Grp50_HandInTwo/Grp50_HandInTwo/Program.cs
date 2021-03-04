f6d97e14168aa047d1cad5e4153b0de8eeba5bc5
Peter Gehlert Theilgaard <au603039@uni.au.dk>
2021-03-04 13:27:22 +0100

    Creation of Solution
    New Solution

---
+++ Grp50_HandInTwo/Grp50_HandInTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Grp50_HandInTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
