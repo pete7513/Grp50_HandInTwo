508a0bf944a57d7b2821beb1c1f0cf2429b0ef9f
Frederik Damgaard Laugesen <au651033@uni.au.dk>
2021-03-10 09:40:02 +0100

    Door, RFID-reader methods

--- Grp50_HandInTwo/Business_Logic_Layer/rfidReader.cs
+++ Grp50_HandInTwo/Business_Logic_Layer/rfidReader.cs
@@ -1,3 +1,12 @@
-﻿internal class rfidReader
+﻿public class rfidReader : IReader
 {
+    public void OnRfidRead(int id)
+    {
+
+    }
+}
+
+public interface IReader
+{
+    public void OnRfidRead(int id);
 }
--- Grp50_HandInTwo/Grp50_HandInTwo/Program.cs
+++ Grp50_HandInTwo/Grp50_HandInTwo/Program.cs
@@ -43,5 +43,3 @@ class Program
             } while (!finish);
         }
     }
-
-}
