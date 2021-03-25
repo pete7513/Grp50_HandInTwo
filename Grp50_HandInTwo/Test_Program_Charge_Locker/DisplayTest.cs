9e70157477505c8a0efac02731a01c2a54aaa2a9
Asbjørn Schöneberg Krogh <au651267@uni.au.dk>
2021-03-25 15:40:25 +0100

    Update DisplayDoorTest

--- Grp50_HandInTwo/Test_Program_Charge_Locker/DisplayTest.cs
+++ Grp50_HandInTwo/Test_Program_Charge_Locker/DisplayTest.cs
@@ -10,15 +10,59 @@ namespace Test_Program_Charge_Locker
 {
     class DisplayTest
     {
+        private IDoor _door;
+        private IReader _reader;
+        private IDisplay _uut;
+        private ILog _log;
+        private IChargeControl _chargeControl;
+        private IUsbCharger _usbCharger;
+
         private IDisplay uut;
+
+        private StationControl _stationControl;
 
         [SetUp]
         public void Setup()
         {
-           uut = new Display();
+            _usbCharger = new UsbChargerSimulator();
+            _uut = Substitute.For<IDisplay>();
+            _reader = new rfidReader();
+            _log = new Log_File();
+            _door = new Door();
+
+            _chargeControl = new ChargeControl(_uut, _usbCharger);
+
+            _stationControl = new StationControl(_door, _uut, _reader, _chargeControl, _log);
+
+
+            uut = new Display();
         }
 
-       
+        [Test]
+        public void ConnectPhone_DoorOpenStatusTrue_Called()
+        {
+           //Arrange
+
+           //Act
+           _door.OnDoorOpen();
+
+           //Assert
+
+           _uut.Received(1).ConnectPhone();
+        }
+
+        [Test]
+        public void ConnectPhone_DoorOpenStatusFalse_Called()
+        {
+           //Arrange
+
+           //Act
+           _door.OnDoorClose();
+
+           //Assert
+           _uut.Received(1).ReadRFID();
+        }
+        
         [Test]
         public void RemovePhone()
         {
